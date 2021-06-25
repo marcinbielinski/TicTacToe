7fd7a3823ae4c4c7ed4afe0232f30b82bee2ce51
Sean Lock <howlempyrean@gmail.com>
2021-06-25 15:23:07 +0200

    Fix

--- main.cpp
+++ main.cpp
@@ -7,92 +7,75 @@ using matrix = std::vector<std::vector<char>>;
 class Board {
 protected:
     matrix board;
+
     void generateBoard() { board.resize(rows, std::vector<char>(columns, init)); }
 
-    void setState(int row, int col, char val) {
+    void setState(int row, int col, char player) {
         if ((board[row][col] != 'X') && (board[row][col] != 'O')) {
             while (board.size() <= row) board.emplace_back();
             while (board[row].size() <= col) board[row].push_back(char());
-            board[row][col] = val;
+            board[row][col] = player;
 
         } else { std::cout << "This square is already taken. Choose a different one." << std::endl; }
     }
 
     bool boardIsFull() {
-        std::all_of(board.begin(), board.end(), [](const std::vector<char>& vec) {
+        std::all_of(board.begin(), board.end(), [](const std::vector<char> &vec) {
             return std::all_of(vec.begin(), vec.end(), [](int elem) { return elem == 'O' || elem == 'X'; });
         });
     }
 
     void printBoard() {
-        std::cout << "Current state of the board: " << std::endl;
         for (const auto &row : board) {
             for (const auto &elem : row) std::cout << elem << ' ';
             std::cout << std::endl;
         }
     }
 
-private:
     int rows = 3;
     int columns = 3;
     char init = '-';
 };
 
-class Game : protected Board{
+class Game : protected Board {
 protected:
-    void getInput() {
-        while (state == GameStates::GameOn) {
-            std::cout << "Choose position to play. 1-9" << std::endl;
-            std::cin >> input;
-            if (!input.empty()) {
-                char command = input.c_str()[0];
 
-                if ((command >= '1') && (command <= '9')) {
-                    switch (command) {
-                        case '1' :
-                            setState(0, 0, active_player);
-                            continueRoutine();
-                            break;
-                        case '2' :
-                            setState(0, 1, active_player);
-                            continueRoutine();
-                            break;
-                        case '3' :
-                            setState(0, 2, active_player);
-                            continueRoutine();
-                            break;
-                        case '4' :
-                            setState(1, 0, active_player);
-                            continueRoutine();
-                            break;
-                        case '5' :
-                            setState(1, 1, active_player);
-                            continueRoutine();
-                            break;
-                        case '6' :
-                            setState(1, 2, active_player);
-                            continueRoutine();
-                            break;
-                        case '7' :
-                            setState(2, 0, active_player);
-                            continueRoutine();
-                            break;
-                        case '8' :
-                            setState(2, 1, active_player);
-                            continueRoutine();
-                            break;
-                        case '9' :
-                            setState(2, 2, active_player);
-                            continueRoutine();
-                            break;
-                        default:
-                            std::cout << "You have to put number in range 1-9" << std::endl;
-                            return;
-                    }
-                }
+    void makeMove() {
+        auto chosenPos = getInput();
+        int xRow = (chosenPos / 3);
+        int yCol;
+        if (chosenPos % 3 == 0)
+        {
+            xRow = xRow - 1;
+            yCol = 2;
+        }
+        else
+        {
+            yCol = (chosenPos % 3) - 1;
+        }
+        setState(xRow, yCol, active_player);
+        continueRoutine();
+    }
+
+    int getInput() {
+        std::cout << "Choose a valid position to play." << std::endl;
+        int input;
+        while (state == GameStates::GameOn)
+        {
+            std::cin >> input;
+
+            if ((input >= 1) && (input <= 9))
+            {
+                break;
+            }
+            else
+            {
+                std::cout << "Invalid input!" << std::endl;
             }
         }
+        return input;
     }
+
 
     void continueRoutine() {
         checkForDraws();
@@ -158,6 +141,7 @@ protected:
             announceWinner();
         }
     }
+
     void checkForDraws() {
         if (boardIsFull()) {
             announceDraw();
@@ -165,7 +149,7 @@ protected:
     }
 
     void announceWinner() {
-        std::cout << active_player << " Wins The Game!" << std::endl;
+        std::cout << "Player: " << active_player << " Wins The Game!" << std::endl;
         state = GameStates::Win;
     }
 
@@ -184,21 +168,20 @@ protected:
 
     GameStates state = GameStates::GameOn;
     char active_player{Player1};
-    std::string input;
-
-
 };
+
 class Runner : public Game {
 public:
     Runner() {
         generateBoard();
+        printBoard();
         state = GameStates::GameOn;
         while (state == GameStates::GameOn) {
-            printBoard();
-            getInput();
+            makeMove();
         }
     }
 };
+
 int main() {
     Runner run;
     return 0;
